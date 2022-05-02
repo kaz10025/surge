85e0ad356c721c1bae4291bf8530ef1145486ce8
kaz10025 <57976101+kaz10025@users.noreply.github.com>
2022-05-02 15:31:52 +0800

    Update timercard.js

--- timercard.js
+++ timercard.js
@@ -90,6 +90,7 @@ function icon_color(){
 $done({
 title:title_random(tnumcount(Number(nowlist))),
 icon:icon_now(tnumcount(Number(nowlist))),
+color:icon_color(),
 content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+","+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
 })
 
